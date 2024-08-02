1a78eb957aa287a0fe84e07169d13384b5f6a0ed
KopusAlina <159190118+KopusAlina@users.noreply.github.com>
2024-08-02 23:10:57 +0300

    2

---
+++ src/Character.java
@@ -0,0 +1,85 @@
+import java.util.Random;
+
+public abstract class Character implements Fighter{
+
+    private String name;
+    private int health;
+    private int strength;
+    private int dexterity;
+    private int exp;
+    private int gold;
+
+    public Character(String name, int health, int power, int agility, int exp, int gold) {
+        this.name = name;
+        this.health = health;
+        this.strength = strength;
+        this.dexterity = dexterity;
+        this.exp = exp;
+        this.gold = gold;
+    }
+
+    @Override
+    public int attack() {
+        if (dexterity * 3 > getRandomValue()) return strength;
+        else return  0; //приравнивается к промаху
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getDexterity() {
+        return dexterity;
+    }
+
+    public void setDexterity(int dexterity) {
+        this.dexterity = dexterity;
+    }
+
+    public int getExp() {
+        return exp;
+    }
+
+    public void setExp(int exp) {
+        this.exp = exp;
+    }
+
+    public int getGold() {
+        return gold;
+    }
+
+    public void setGold(int gold) {
+        this.gold = gold;
+    }
+
+    private int getRandomValueSucksess() {
+        return (int) (Math.random() * 100);
+    }
+
+    private int getRandomValueKrit
+
+    @Override
+    public String toString() {
+        return String.format("%s здоровье:%d", name, health);
+    }
+}
---
+++ src/Dealer.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Dealer {
+    Dealer bag = new Dealer(){
+        int zellieDealer = 100;
+    }
+    void ZellieDo(){
+        int health + 10;
+    }
+    void Order() {
+        if (Scanner = "купить зелье"){
+            int zellieDealer --;
+            int zellieUser ++;
+        }
+    }
+}
